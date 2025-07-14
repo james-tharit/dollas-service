cd3cdab1d7d55ca2c5bdd48afc3977b016163010
mr.frog <tharit.rt@gmail.com>
2025-07-14 22:38:34 +0700

    feat: added authentication

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use axum::{
     Json, Router,
-    http::StatusCode,
+    http::{Method, StatusCode, header::CONTENT_TYPE},
     routing::{get, post},
 };
+
 use serde::{Deserialize, Serialize};
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 
@@ -19,10 +22,19 @@ async fn main() {
     struct AuthResponse {
         token: bool,
     }
+
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any)
+        .allow_headers([CONTENT_TYPE]);
+
     // our router
     let app = Router::new()
         .route("/", get(health_check))
-        .route("/authenticate", post(validate_password));
+        .route("/authenticate", post(validate_password))
+        .layer(ServiceBuilder::new().layer(cors));
 
     // which calls one of these handlers
     async fn health_check() -> &'static str {
@@ -32,6 +44,7 @@ async fn main() {
     async fn validate_password(
         Json(payload): Json<AuthRequest>,
     ) -> Result<Json<AuthResponse>, StatusCode> {
+        println!("Request coming in");
         println!("{:?}", payload);
         if payload.secret_keyword.to_uppercase() == SECRET_KEYWORD {
             Ok(Json(AuthResponse { token: true }))
